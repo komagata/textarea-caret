eec472d4530f87d9440ddb8f48bee9a3a04743ee
Masaki Komagata <komagata@gmail.com>
2017-08-15 12:22:30 +0900

    Add basic methods.

--- index.js
+++ index.js
@@ -1,34 +1,106 @@
-module.exports = {
-  indexes: function(el) {
-    var lines = this.lines(el);
-    var indexes = [];
+(function() {
+  function TextareaCaret(textarea) {
+    this.textarea = textarea;
+  }
+
+  TextareaCaret.prototype.caretPosition = function() {
+    return this.textarea.selectionStart;
+  };
+
+  TextareaCaret.prototype.currentHeadIndex = function() {
+    var parsedLines   = this.parsedLines();
+    var caretPosition = this.caretPosition();
+
+    if (parsedLines.length < 2) return 0;
+
+    for (var i = 0; i < parsedLines.length; i++) {
+      var line = parsedLines[i];
+
+      if (line.start <= caretPosition && caretPosition < line.end) {
+        return line.start;
+      }
+    }
+    return parsedLines[parsedLines.length - 1].start;
+  };
+
+  TextareaCaret.prototype.currentLine = function() {
+    var parsedLines   = this.parsedLines();
+    var caretPosition = this.caretPosition();
+
+    for (var i = 0; i < parsedLines.length; i++) {
+      var line = parsedLines[i];
+
+      if (line.start <= caretPosition && caretPosition < line.end) {
+        return line.value;
+      }
+    }
+    return this.textarea.value;
+  };
+
+  TextareaCaret.prototype.headIndexes = function() {
+    var lines = this.lines();
+    var headIndexes = [];
     var pos = 0;
-    for (var i = 0; i <= lines.length; i++) {
-      indexes.push(pos);
+
+    if (lines.length < 2) return [0];
+
+    for (var i = 0; i < lines.length; i++) {
       var line = lines[i];
+      headIndexes.push(pos);
       pos += (line + "\n").length
     }
-    return indexes;
-  },
-  lines: function(el) {
-    return el.value.split("\n");
-  },
-  caretPosition: function(el) {
-    return el.selectionStart;
-  },
-  insert: function(el, text, position) {
-    el.value = el.value.substr(0, position) + text + el.value.substr(position, el.value.length);
-  },
-  insertAtCaret: function(el, text) {
-    this.insert(el, text, this.caretPosition(el));
+    return headIndexes;
+  };
+
+  TextareaCaret.prototype.insert = function(text, position) {
+    var before = this.textarea.value.substr(0, position);
+    var after  = this.textarea.value.substr(position, this.textarea.value.length);
+    this.textarea.value = before + text + after;
+  };
+
+  TextareaCaret.prototype.insertAtCaret = function(text) {
+    this.insert(text, this.caretPosition());
+  };
+
+  TextareaCaret.prototype.insertAtCurrentHead = function(text) {
+    this.insert(text, this.currentHeadIndex());
+  };
+
+  TextareaCaret.prototype.lines = function() {
+    if (/\n/.test(this.textarea.value)) {
+      return this.textarea.value.split("\n");
+    } else {
+      return [];
+    }
+  };
+
+  TextareaCaret.prototype.parsedLines = function() {
+    var lines = this.lines();
+    var parsedLines = [];
+    var pos = 0;
+
+    for (var i = 0; i < lines.length; i++) {
+      var line = lines[i];
+      var lf = i === lines.length - 1 ? "" : "\n";
+      var value = line + lf;
+      var length = value.length;
+      var end = pos + length;
+
+      parsedLines.push({
+        start:  pos,
+        end:    end,
+        length: length,
+        value:  value
+      });
+
+      pos += length;
+    }
+    return parsedLines;
+  };
+
+  if (typeof module === 'object') {
+    module.exports = TextareaCaret;
+  } else {
+    window.TextareaCaret = TextareaCaret;
   }
-};
-
-
-function TextareaCaret(el) {
-  this.el = el;
-}
-
-TextareaCaret.prototype.aaa = function(arg) {
-  return "bbb";
-};
+})();
--- test.js
+++ test.js
@@ -1,33 +1,126 @@
 var assert = require("assert");
-var util = require("./index.js");
+var TextareaCaret = require("./index.js");
 
 describe("textarea-caret-util", function() {
   var textarea = null;
+  var tc = null;
 
   beforeEach(function(){
     document.body.innerHTML = "<!DOCTYPE html><html lang=\"ja\"><head><meta charset=\"UTF-8\"><title>Test</title></head><body><textarea></textarea></body></html>";
     textarea = document.querySelector("textarea");
-  });
-
-  it("lines", function() {
-    textarea.value = "aaa\nbbb\nccc";
-    assert.deepEqual(util.lines(textarea), ["aaa", "bbb", "ccc"]);
+    tc = new TextareaCaret(textarea);
   });
 
   it("caretPosition", function() {
-    assert.equal(util.caretPosition(textarea), 0);
+    assert.equal(tc.caretPosition(), 0);
+  });
+
+  it("currentHeadIndex", function() {
+    textarea.value = "";
+    textarea.setSelectionRange(0, 0);
+    assert.equal(tc.currentHeadIndex(), 0);
+
+    textarea.value = "aaa\nbbb\nccc";
+    textarea.setSelectionRange(8, 8);
+    assert.equal(tc.currentHeadIndex(), 8);
+
+    textarea.value = "aaa\nbbb\nccc";
+    textarea.setSelectionRange(5, 5);
+    assert.equal(tc.currentHeadIndex(), 4);
+
+    textarea.value = "aaa\nbbb\nccc";
+    textarea.setSelectionRange(11, 11);
+    assert.equal(tc.currentHeadIndex(), 8);
+  });
+
+  it("currentLine", function() {
+    textarea.value = "";
+    textarea.setSelectionRange(0, 0);
+    assert.equal(tc.currentLine(), "");
+
+    textarea.value = "aaa\nbbb\nccc";
+    textarea.setSelectionRange(8, 8);
+    assert.equal(tc.currentLine(), 'ccc');
+
+    textarea.value = "aaa\nbbb\nccc";
+    textarea.setSelectionRange(5, 5);
+    assert.equal(tc.currentLine(), "bbb\n");
+
+    textarea.value = "aaa\n## bbb\nccc";
+    textarea.setSelectionRange(8, 8);
+    assert.equal(tc.currentLine(), "## bbb\n");
+  });
+
+  it("headIndexes", function() {
+    textarea.value = "aaa\nbbb\nccc";
+    assert.deepEqual(tc.headIndexes(), [0, 4, 8]);
+
+    textarea.value = "";
+    assert.deepEqual(tc.headIndexes(), [0]);
   });
 
   it("insert", function() {
     textarea.value = "aaa\nbbb\nccc";
-    util.insert(textarea, "XXX\n", 4);
+    tc.insert("XXX\n", 4);
     assert.equal(textarea.value, "aaa\nXXX\nbbb\nccc");
   });
 
   it("insertAtCaret", function() {
     textarea.value = "aaa\nbbb\nccc";
     textarea.setSelectionRange(8, 8);
-    util.insertAtCaret(textarea, "XXX\n");
+    tc.insertAtCaret("XXX\n");
     assert.equal(textarea.value, "aaa\nbbb\nXXX\nccc");
+  });
+
+  it("insertAtCurrentHead", function() {
+    textarea.value = "aaa\nbbb\nccc";
+    textarea.setSelectionRange(8, 8);
+    tc.insertAtCurrentHead("## ");
+    assert.equal(textarea.value, "aaa\nbbb\n## ccc");
+
+    textarea.value = "aaa\nbbb\nccc";
+    textarea.setSelectionRange(5, 5);
+    tc.insertAtCurrentHead("- ");
+    assert.equal(textarea.value, "aaa\n" + '- ' + "bbb\nccc");
+
+    textarea.setSelectionRange(5, 5);
+    tc.insertAtCurrentHead("- ");
+    assert.equal(textarea.value, "aaa\n" + '- - ' + "bbb\nccc");
+  });
+
+  it("lines", function() {
+    textarea.value = "";
+    assert.deepEqual(tc.lines(), []);
+
+    textarea.value = "aaa\nbbb\nccc";
+    assert.deepEqual(tc.lines(), ["aaa", "bbb", "ccc"]);
+  });
+
+  it("parsedLines", function() {
+    textarea.value = "";
+    assert.deepEqual(tc.parsedLines(), []);
+
+    textarea.value = "aaa\nbbb\nccc";
+    var expected = [
+      {
+        start:  0,
+        end:    4,
+        length: 4,
+        value:  "aaa\n"
+      },
+      {
+        start:  4,
+        end:    8,
+        length: 4,
+        value:  "bbb\n"
+      },
+      {
+        start:  8,
+        end:    11,
+        length: 3,
+        value:  "ccc"
+      }
+    ];
+    assert.deepEqual(tc.parsedLines(), expected);
   });
 });
